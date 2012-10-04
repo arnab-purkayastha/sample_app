55f6c24bbf33fe5ccb06e1dffc1e74799d2c58f5
arnab-purkayastha <hp4me06@gmail.com>
2012-10-03 23:14:58 -0600

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,45 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text => "Ruby on Rails Tutorial | Home")
+    end
   end
+
+  describe "Stuff" do
+
+  it "should have the content 'Stuff'" do
+    visit '/static_pages/stuff'
+    page.should have_content('Stuff')
+  end
+  it "should have the right title" do
+    visit '/static_pages/stuff'
+    page.should have_selector('title',:text=>"Ruby on Rails Tutorial | Stuff")
+  end
+end
+
+  describe "Help page" do
+
+  it "should have the content 'Help'" do
+    visit '/static_pages/help'
+    page.should have_content('Help')
+  end
+  it "should have the right title" do
+    visit '/static_pages/help'
+    page.should have_selector('title',:text=>"Ruby on Rails Tutorial | Help")
+  end
+end
+
+  describe "About page" do
+    
+  it "should have the content 'About'" do
+    visit '/static_pages/about'
+    page.should have_content('About')
+  end
+  it "should have the right title" do
+    visit '/static_pages/about'
+    page.should have_selector('title',:text=>"Ruby on Rails Tutorial | About")
+  end
+end
 end
